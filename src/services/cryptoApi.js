2c0048464af03977ef66ef02441037164064b33b
am-miracle <judemiraco0@gmail.com>
2022-01-30 23:58:37 +0100

    update api keys

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,15 +5,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // const baseUrl = process.env.COINRANKING_RAPID_API_URL;
 
 const cryptoApiHeader = {
-  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': '129b2fc319msh341b23a0412e0abp1dab02jsn64e71a25960c'
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  'x-rapidapi-key': process.env.REACT_APP_CRYPTO_API_KEY
 };
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeader});
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://coinranking1.p.rapidapi.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -5,15 +5,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeader = {
     'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '129b2fc319msh341b23a0412e0abp1dab02jsn64e71a25960c'
+    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_NEWS_API_KEY
 };
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeader});
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://bing-news-search1.p.rapidapi.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
   endpoints: (builder) => ({
     getCryptosNews: builder.query({
       query: ({newsCategory, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
